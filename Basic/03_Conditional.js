75ac59e75e2be1c52c231f8acbd22d1fe7c98675
ches0703 <ches0703@naver.com>
2023-01-19 21:52:40 +0900

    JavaScript 조건문

---
+++ Basic/03_Conditional.js
@@ -0,0 +1,14 @@
+let isShow = true
+let checked = false
+
+if(isShow){
+    console.log("Show");    // -> Show
+}
+
+if(checked){
+    console.log("Checked");
+}
+else{
+    console.log("Not Checked")  // -> Not Checked
+}
+
